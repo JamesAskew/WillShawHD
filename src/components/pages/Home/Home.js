621b1ae350c4d0c1f0004e557b718c49cf6986a9
James Askew <james.askew@netcel.com>
2019-01-11 17:27:54 +0000

    conert hero portfolio to work with reactjs

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Header from "../../Header/Header";
+import PortfolioHeader from "../../Header/PortfolioHeader/PortfolioHeader";
 
 const Home = () => {
   return (
     <div>
       <Header>
         <p>Children go here</p>
+        <PortfolioHeader />
       </Header>
     </div>
   );
